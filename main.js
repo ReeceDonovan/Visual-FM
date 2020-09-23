4c21eb3b9a1ed05b3b5bf5f452b8184bccee7eed
Reece Donovan <reecedonovan99@gmail.com>
2020-09-23 15:00:18 +0100

    Implementing JS dynamic tabs and fetch api calls

--- main.js
+++ main.js
@@ -1,70 +1,61 @@
-let running;
-let container = document.getElementById("imageContainer");
-function sleep(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
-
-function createRow() {
-  for (i = 1; i < 8; i++) {
-    let row = document.createElement("div");
-    row.id = "row " + String(i);
-    row.className = "row";
-    container.appendChild(row);
-  }
-}
-
-async function showData(data) {
-  running++;
-  if (running <= 1) {
-    container.innerHTML = "";
-    createRow();
-    let j = 1;
-
-    for (let i = 0; i < data.length - 1; i++) {
-      let curRow = document.getElementById("row " + String(j));
-      let a = "";
-      let img = "";
-      a = document.createElement("a");
-      img = new Image();
-      img.src = data[i].image[3]["#text"];
-      if (data[i].image[3]["#text"] == "") {
-        img.src = "";
-        img.src =
-          "https://lastfm.freetls.fastly.net/i/u/174s/2a96cbd8b46e442fc41c2b86b821562f.png";
-      }
-
-      a.appendChild(img);
-      a.href = data[i].artist.url;
-      a.className = "column";
-
-      curRow.appendChild(a);
-      if (curRow.childElementCount >= 7) {
-        j++;
-      }
-      await sleep(50);
-    }
-  }
-}
-
-function getFMData() {
-  running = 0;
-  let usernameInput = document.getElementById("username");
-  let albumData;
-  let username = usernameInput.value;
-
-  let http = new XMLHttpRequest();
-  let url =
-    "https://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=" +
+//Debug Elements
+//
+//UI Elements
+let usernameInput = document.getElementById("username");
+let username = usernameInput.value;
+let startBtn = document.getElementById("searchBtn");
+//Page Elements
+let cont = document.getElementById("infoContainer");
+//API Elements
+let urls = [
+  "https://ws.audioscrobbler.com/2.0/?method=user.gettoptracks&user=" +
     username +
-    "&api_key=690f24077f81c36f5c08ad294a858822&period=overall&format=json";
-  http.open("GET", url);
-  http.send();
+    "&api_key=690f24077f81c36f5c08ad294a858822&period=overall&format=json",
+  "https://ws.audioscrobbler.com/2.0/?method=user.gettopalbums&user=" +
+    username +
+    "&api_key=690f24077f81c36f5c08ad294a858822&period=overall&format=json",
+  "https://ws.audioscrobbler.com/2.0/?method=user.gettopartists&user=" +
+    username +
+    "&api_key=690f24077f81c36f5c08ad294a858822&period=overall&format=json",
+];
 
-  http.onreadystatechange = (e) => {
-    let apiData = http.responseText;
-    let parsedData = JSON.parse(apiData);
-    albumData = parsedData.topalbums.album;
+startBtn.addEventListener("click", getFMData);
 
-    showData(albumData);
-  };
+async function getFMData() {
+  const results = await Promise.all(
+    urls.map((url) => fetch(url).then((r) => r.json()))
+  );
+  formatData(results);
+}
+
+function formatData(d) {
+  const topTrackData = d[0].toptracks.track;
+  const topAlbumData = d[1].topalbums.album;
+  const topArtistData = d[2].topartists.artist;
+
+  console.log(topTrackData);
+  console.log(topAlbumData);
+  console.log(topArtistData);
+  genTabs();
+}
+
+function genTabs() {
+  cont.innerHTML = "";
+  let tabTitleNames = ["Top Tracks", "Top Albums", "Top Artists"];
+  let tabTitleLinks = ["#trackTab", "#albumTab", "#artistTab"];
+  let ul = document.createElement("ul");
+  ul.id = "nav-tab";
+  ul.className = "nav";
+  for (let i = 0; i < tabTitleLinks.length; i++) {
+    let li = document.createElement("li");
+    if (i == 0) {
+      li.className = "active";
+    }
+    let a = document.createElement("a");
+    a.href = tabTitleLinks[i];
+    a.innerHTML = tabTitleNames[i];
+    li.appendChild(a);
+    ul.appendChild(li);
+  }
+  cont.appendChild(ul);
 }
